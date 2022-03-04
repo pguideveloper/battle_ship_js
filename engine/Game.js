97e572c2c47a89429b6932b36746c647c5e32f7f
Pedro Henrique Guimarães <pedroguimaraes.contato@gmail.com>
2022-03-04 16:36:06 -0300

    Now board receive a ship object

--- engine/Game.js
+++ engine/Game.js
@@ -2,18 +2,18 @@ class Game {
     board;
     ship;
     start() {
-
         this.board = new Board(10);
-        this.board.draw();
+        this.board.draw_board();
         this.board.draw_attack_board();
         
-        this.ship = new Ship(this.board);    
+        this.ship = new Ship(this.board);
+        this.board.setShip(this.ship);
     }
 
     add_ship(start, position, type) {
         this.ship.draw(start, position, type);
     }
 
-    
+
     
 }
